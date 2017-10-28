cbf4f1e3515ff8f7caab16c443b5b47020a01f33
Maury Viana <mauryrv@gmail.com>
2017-10-28 00:23:41 -0200

    Finish PHP project

--- Php/DM107FinalProject/src/public/index.php
+++ Php/DM107FinalProject/src/public/index.php
@@ -1,94 +1,80 @@
 <?php 
 
-    use \Psr\Http\Message\ServerRequestInterface as Request;
-    use \Psr\Http\Message\ResponseInterface as Response;
-    
-    require '../vendor/autoload.php';
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
-$config['displayErrorDetails'] = true; 
-   $config['addContentLengthHeader'] = false;
+require 'config.php';
 
-    $config['db']['host'] = "localhost"; 
-    $config['db']['user'] = "root"; 
-    $config['db']['pass'] = "root"; 
-    $config['db']['dbname'] = "TrabalhoDM107";
-
-    $app = new \Slim\App(["config" => $config]);
-    $container = $app->getContainer();
-
-    $container['db'] = function ($c) { 
-        $dbConfig = $c['config']['db']; 
-        $pdo = new PDO("mysql:host=" . $dbConfig['host'] . ";dbname=" . $dbConfig['dbname'], 
-            $dbConfig['user'], $dbConfig['pass']); 
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); 
-        $db = new NotORM($pdo); 
-        return $db; 
-    };
-$app->get('/api/getNome/{nome}', function (Request $request, Response $response) {
+//get delivery - used for test
+$app->get('/api/delivery/{nome}', function (Request $request, Response $response) {
         $nome = $request->getAttribute('nome');
-        $response->getBody()->write("Bem vindo a API, $nome");
+        $entrega = $this->db->delivery("id=?",$nome)->fetch();
         
-        return $response;
+        if(empty($entrega)){
+            return $response->withJson("{Status:Delivery not found}", 404);
+        }
+
+        return $response->withJson($entrega);
     });
 
+    //update delivery
    $app->put('/api/delivery', function (Request $request, Response $response) {
         $body = $request->getBody();
 
-         if(empty($body["id"])){
-            return $response->withJson("Id obrgatório!");
-        }
-        $id = $body["id"];
-
-        if(empty($body["nome"])){
-            return $response->withJson("Nome obrgatório!");
-        }
-        $nome = $body["nome"];
-
-         if(empty($body["cpf"])){
-            return $response->withJson("Cpf obrgatório!");
-        }
-         $cpf = $body["cpf"];
-
-        if(empty($body["data"])){
-            return $response->withJson("Data e hora obrgatório no formato dd/mm/yyyy hh:mm ss!");
-        }
-         $data = $body["data"];
-
-       //checa se existe a entrega
-        $entrega = $this->db->delivery("id=?",$idEntrega)->fetch();
+        //Json format example
+       /*{
+            "id":"2",
+            "nome":"nome entregador",
+            "cpf":"00000000000",
+            "dataDelivery":"10/12/2020 23:49:34"
+          }*/
+        $data = json_decode($body); 
+   
+        $id = $data->id;
+        $nome = $data->nome;
+        $cpf = $data->cpf;
+        $dateDelivery = $data->dataDelivery;
+       
+       //check if the delivery exists
+        $entrega = $this->db->delivery("id=?",$id)->fetch();
 
         if(empty($entrega)){
             return $response->withStatus(404);
         }
-        //update a entrega com os itens necessários
+        //update the delivery
         $entregaUpdate =array(
         "receiver_name" => $nome,
         "receiver_cpf" => $cpf,
-        "delivery_date" => $data
+        "delivery_date" => date("Y-m-d H:i:s",strtotime(str_replace('/','-',$dateDelivery)))
         );
-        $result = $entrega->update($entregaUpdate);
         
-        //pega a entrega depois de atualizada e mostra a mesma
+        $resultUpdate = $entrega->update($entregaUpdate);
+
+        if($resultUpdate == 0){
+            return $response->withStatus(500);
+        }
+
+        //get and show the delivery updated
         $entrega = $this->db->delivery("id=?",$id)->fetch();
         
         return $response->withJson($entrega);
     });
 
+    //delete delivery
     $app->delete('/api/delivery/{idEntrega}', function (Request $request, Response $response) {
        
         $idEntrega = $request->getAttribute("idEntrega");
-        //checa se existe a entrega
+        //check if the delivery exists
         $entrega = $this->db->delivery("id=?",$idEntrega)->fetch();
         if(empty($entrega)){
             return $response->withStatus(404);
         }
-        //delete a entrega
+        //delete delivery
         $this->db->delivery("id=?",$idEntrega)->delete(); //verificar modo correto de fazer
 
 
        return $response->withJson("{Delete:ok}", 200);
-        //return $response->withJson($tarefas);
+
     });
 
 
